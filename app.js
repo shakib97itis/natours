0a604e15bbb76136777400b85c394a1434c1b7b9
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-11-17 21:06:07 +0600

    refactor: update API endpoint to serve tours data from JSON file

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
 const express = require('express');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
 const PORT = process.env.PORT || 3000;
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
