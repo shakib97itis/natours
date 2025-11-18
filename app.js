aba5b54a119971726ae41efa673b205783fd31df
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-11-18 22:40:25 +0600

    read url params, express.json middleware added

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const tours = JSON.parse(
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours', (_req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -16,6 +17,51 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours, null, 2),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Could not save the new tour',
+        });
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {
