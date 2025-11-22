cf5804135e02fb360cd1e873d1478aaeaa2ef350
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-11-22 10:46:04 +0600

    Morgan added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const morgan = require('morgan');
 
 const app = express();
 
-// Middlewares
+//Global Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
-
-// Todo: install and setup morgan.
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
