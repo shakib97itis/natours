484bbdc6339000417be48a0f41bb6effb77efcdf
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-11-27 21:53:55 +0600

    Refactor tour controller to use async/await and improve error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,24 +1,55 @@
-exports.getAllTours = (_req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: '',
-    data: {},
-  });
+const Tour = require('../models/tourModel');
+
+exports.getAllTours = async (_req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Something went wrong',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // const { id } = req.params;
-  res.status(200).json({
-    status: 'success',
-    data: {},
-  });
+exports.getTour = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Something went wrong',
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
+  const { name, price, rating } = req.body;
   try {
+    const newTour = await Tour.create({
+      name,
+      price,
+      rating,
+    });
+
     res.status(201).json({
       status: 'success',
-      data: {},
+      data: {
+        tour: newTour,
+      },
     });
   } catch (err) {
     res.status(400).json({
@@ -28,16 +59,41 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: 'updated tour data',
-  });
+exports.updateTour = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  try {
+    const tour = await Tour.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      tour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Something went wrong',
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: 'no data',
-  });
+exports.deleteTour = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Tour.findByIdAndDelete(id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Something went wrong',
+    });
+  }
 };
