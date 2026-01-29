162189fc76b7207d2bbad91473efa0896e1dfcdb
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-29 13:10:08 +0600

    fix: simplify 404 error message in app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/api/v1/users', userRouter);
 app.all('*', (req, res) => {
   res.status(404).json({
     status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+    message: `Can't find on this server!`,
   });
 });
 
