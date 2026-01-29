1e1b14d0d49f80da51114107174e9df634ec7f6c
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-29 12:51:52 +0600

    refactor: reorganize middleware and route handling in app.js

--- app.js
+++ app.js
@@ -14,14 +14,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Middleware
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
 
+// Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handle unhandled routes
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
