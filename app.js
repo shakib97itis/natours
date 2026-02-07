cd9bc25666e28ea1983848c1744798fc9c3a9cfb
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-02-07 16:30:15 +0600

    fet: add rat limit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -14,6 +16,14 @@ app.set('query parser', 'extended');
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 // Middleware
 app.use(express.json());
